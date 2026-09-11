30fbf3f25a1b8adca41a23f1404dfa4e08d5d63e
Anand raj <connect.anandraj@gmail.com>
2026-09-11 11:10:37 +0530

    Add basic widget test scaffold

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:habitloop/main.dart';
+
+void main() {
+  testWidgets('App renders home screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const HabitLoopApp());
+    expect(find.text('HABITLOOP'), findsOneWidget);
+  });
+}
